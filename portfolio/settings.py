a461e618c9c5ee26ab854c1871e67762a76b05bf
GOLLAPROLU-VENKATESH <63569956+GOLLAPROLU-VENKATESH@users.noreply.github.com>
2021-06-15 21:53:30 +0530

    heruko commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z#+2v4j9g6(fca&kdz&3k8swahs0m!!)%wi96!+^+b@14$zwzp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'gollaprolu-venkatesh-protfolio.herokuapp.com', 'localhost']
 
 
 # Application definition

9cd5ffd16f67cd52b6577b1e7b2f6549716dd6c7
GOLLAPROLU-VENKATESH <63569956+GOLLAPROLU-VENKATESH@users.noreply.github.com>
2021-06-15 21:59:23 +0530

    commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -125,7 +125,8 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),

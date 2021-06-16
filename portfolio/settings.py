b82ccd0f16f6341c735ca52ed60aafc8b6e1d9e2
GOLLAPROLU-VENKATESH <63569956+GOLLAPROLU-VENKATESH@users.noreply.github.com>
2021-06-16 08:18:04 +0530

    commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import django_heroku,dj_database_url
-
+import cloudinary
+import cloudinary_storage
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'
+    'app',
+    'cloudinary',
+    'cloudinary_storage',
 ]
 
 MIDDLEWARE = [
